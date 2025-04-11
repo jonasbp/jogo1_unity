521cac4dd01e5ac2f751a7fe6b538b2f10206200
jonasbp <jonasbonfapelegrina@gmail.com>
2025-04-10 21:05:21 -0300

    Github

---
+++ Assets/GameController.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public static class GameController
+{
+    private static int collectableCount;
+
+    public static bool gameOver{
+        get { return collectableCount <= 0; }
+    }
+
+    public static void Init()
+    {
+        collectableCount = 7;
+    }
+    public static void Collect()
+    {
+        collectableCount--;
+    }
+
+}
